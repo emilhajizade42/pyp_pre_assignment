57ad67bc61c3c2b6549c4c057c671649c1629b9f
emilhajizade <emilhacizade44@mail.ru>
2022-08-17 00:47:13 +0400

    Added validation

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -9,7 +9,31 @@ const subscribeModal = document.querySelector("#subscribe");
 const mainSliderImg = document.querySelector(".mainSliderImg>img");
 const mainSliderImgTitle = document.querySelector(".mainSliderImg>span");
 const imgboxImgsArr = document.querySelectorAll(".imgbox>img");
-
+//////////inpCheckIn
+const navInputs = document.querySelectorAll("nav input");
+const navButton = document.querySelector("nav button");
+const contactInputs = document.querySelectorAll("#contact input");
+const contactButton = document.querySelector("#contact button");
+const subscribeInputs = document.querySelectorAll("#subscribe input");
+const subscribeButton = document.querySelector("#subscribe button");
+navButton.addEventListener("click",function(e){
+  e.preventDefault()
+  navInputs.forEach(inp=>{
+    validateInputAll(inp)
+  })
+})
+contactButton.addEventListener("click",function(e){
+  e.preventDefault()
+  contactInputs.forEach(inp=>{
+    validateInputAll(inp)
+  })
+})
+subscribeButton.addEventListener("click",function(e){
+  e.preventDefault()
+  subscribeInputs.forEach(inp=>{
+    validateInputAll(inp)
+  })
+})
 // Sidebar mobil open
 headerMenuBtn.addEventListener("click",function (e) {
     nav.style.display="block";
@@ -39,13 +63,39 @@ subscribeLinks.forEach(item =>{
 const roomNames = ["Living Room","Dining Room","Bedroom","Living Room II"];
 imgboxImgsArr.forEach((img,index)=>{
   img.addEventListener("click",function(e){
-    RemoveClassAll(imgboxImgsArr,"active")
+    removeClassAll(imgboxImgsArr,"active")
     e.target.classList.add("active")
     mainSliderImgTitle.innerHTML=roomNames[index]
     mainSliderImg.setAttribute("src", e.target.getAttribute("src"))
   })
 })
 // Remove all classes from element array
-function RemoveClassAll(elementArr,className) { 
+function removeClassAll(elementArr,className) { 
   elementArr.forEach(element=>element.classList.remove(className))
  }
+ function validateInputAll(element) {
+    const value = element.value;
+    const inputWarning = element.nextSibling.nextSibling;
+    if (value.trim() ==="") {
+      inputWarning.innerHTML ="This field is required"
+      inputWarning.style.display="block";
+    }
+    else if(element.getAttribute("type")==="num" && !(/^\d+\.?\d*$/).test(value)){
+      inputWarning.innerHTML ="Format is not vaild"
+      inputWarning.style.display="block";
+    }
+    else if(element.getAttribute("type")==="email" && !value.includes("@")){
+      inputWarning.innerHTML ="Email is not vaild"
+      inputWarning.style.display="block";
+    }
+    else if(element.getAttribute("type")==="datetime" && !(/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/).test(value)){
+      inputWarning.innerHTML ="Date is not vaild"
+      inputWarning.style.display="block";
+    }
+    else{
+      inputWarning.style.display="none";
+    }
+ }
+
+//  var zad = (/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/).test("08/08/2013")
+//  alert(zad)
